ca0d345a42d73167e2ffed30a01148dc62c4e017
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2021-04-20 17:29:32 +0200

    added a constructor method

--- nolistfs.go
+++ nolistfs.go
@@ -1,7 +1,7 @@
-// NoListFileSystem is a custom filesystem implementation. It wraps
-// around a "base" file system. It is mainly used with
-// http.FileServer so that 404 status code is returned instead of
-// a directory listing.
+// NoListFileSystem is a custom filesystem implementation. It
+// follows the decorator pattern and wraps around a "base"
+// file system. It is mainly used with http.FileServer so that
+// 404 status code is returned instead of a directory listing.
 package nolistfs
 
 import (
@@ -14,8 +14,19 @@ type NoListFile struct {
 	http.File
 }
 
+// Readdir implements the Readdir function of http.FileSystem.
+// In this specific case, it will always return nil, nil.
 func (f NoListFile) Readdir(count int) ([]os.FileInfo, error) {
 	return nil, nil
+}
+
+// New is a constructor method which returns the initialized
+// No Listing File System.
+func New(base http.FileSystem) http.FileSystem {
+	nfs := &NoListFileSystem{
+		base: base,
+	}
+	return nfs
 }
 
 // NoListFileSystem is a custom filesystem implementation. It wraps
@@ -26,6 +37,8 @@ type NoListFileSystem struct {
 	base http.FileSystem
 }
 
+// Open implements Open method required by http.FileSystem. This
+// function returns a 404 error when a trailing "/" is called.
 func (nfs NoListFileSystem) Open(path string) (http.File, error) {
 	f, err := nfs.base.Open(path)
 	if err != nil {
